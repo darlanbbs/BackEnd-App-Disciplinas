9cf07b8ddb096671dcfb364f27caff2a31fb2f25
Darlan Bomfim <darlanbs05@gmail.com>
2023-09-15 10:37:43 -0300

    Adicionar Cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const Router = require("./routes/Router");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use(Router);
 app.listen(3000);
