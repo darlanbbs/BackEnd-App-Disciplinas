cd394883838beab1080d4b41670ac1e8c33b4c04
Darlan Bomfim <darlanbs05@gmail.com>
2023-09-15 08:58:18 -0300

    Feat:Adicionar disciplina

--- src/controllers/resultsControllers.js
+++ src/controllers/resultsControllers.js
@@ -15,7 +15,34 @@ function getDisciplina(req, res) {
   res.send(disciplina);
 }
 
+function addDisciplina(req, res) {
+  const now = new Date();
+
+  const { bimestre, nota, disciplina } = req.body;
+
+  if (!bimestre || !nota || !disciplina) {
+    return res.status(400).json({ message: "Preencha todos os campos." });
+  }
+
+  if (nota < 0 || nota > 10) {
+    return res.status(400).json({ message: "Nota inválida." });
+  }
+
+  const novaAvaliacao = {
+    id: uuid.v4(),
+    disciplina,
+    bimestre,
+    nota,
+    criado_em: `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`,
+    atualizado_em: `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`,
+  };
+
+  disciplinas.push(novaAvaliacao);
+  res.status(200).send("Bimestre adicionado com sucesso!");
+}
+
 module.exports = {
   getDisciplinas,
   getDisciplina,
+  addDisciplina,
 };
