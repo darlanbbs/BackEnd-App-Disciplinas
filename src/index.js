e5a5fd59ff815e4ad7d02c448e42cf22031d85d8
Darlan Bomfim <darlanbs05@gmail.com>
2023-10-14 19:31:24 -0300

    Fix:mudança de rota

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const Router = require("./routes/Router");
 const cors = require("cors");
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 app.use(Router);
-app.listen(3000);
+app.listen(process.env.PORT);
